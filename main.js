7bb17b7e59b9eedf23381798f33980443411e4bf
nison79 <georgenison@gmail.com>
2020-10-17 21:34:59 +0300

    first

---
+++ main.js
@@ -0,0 +1,5 @@
+
+
+gsap.to(".card__main", { duration: 1,runBackwards: true, ease: "power2.out", y: -500 });
+gsap.to("#primary", { duration: 1,runBackwards: true, ease: "power2.out", y: -500 , x:-200 });
+
